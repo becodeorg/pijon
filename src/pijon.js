3d4cb525f3b9b93f0ab1638f08497c1500189a8a
leny <info@flatland.be>
2019-07-20 17:04:33 +0200

    Main implementation + upload to S3

--- src/pijon.js
+++ src/pijon.js
@@ -8,10 +8,25 @@
 
 import sls from "serverless-http";
 import Koa from "koa";
+import koaBody from "koa-body";
 import cors from "koa2-cors";
+import * as AWS from "aws-sdk";
+import {createReadStream} from "fs";
+import {getType} from "mime/lite";
+import uuid from "uuid/v5";
+
+const IS_OFFLINE = process.env.IS_OFFLINE === "true";
+const LOCAL_S3_ENDPOINT = "http://localhost:6071";
+const AWS_REGION = "eu-west-1";
+const {
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_S3_BUCKET_NAME,
+} = process.env;
 
 const app = new Koa();
 
+app.use(koaBody({multipart: true}));
 app.use(async (ctx, next) => {
     try {
         await next();
@@ -26,12 +41,43 @@ app.use(async (ctx, next) => {
 app.use(
     cors({
         origin: "*",
-        allowMethods: ["GET", "HEAD", "OPTIONS"],
+        allowMethods: ["POST", "HEAD", "OPTIONS"],
     }),
 );
 
-app.use(ctx => {
-    ctx.body = "Hello, world!";
+app.use(async ctx => {
+    const client = new AWS.S3(
+        IS_OFFLINE
+            ? {
+                  accessKeyId: "S3RVER",
+                  secretAccessKey: "S3RVER",
+                  s3ForcePathStyle: true,
+                  endpoint: new AWS.Endpoint(LOCAL_S3_ENDPOINT),
+              }
+            : {
+                  accessKeyId: AWS_ACCESS_KEY_ID,
+                  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+                  region: AWS_REGION,
+              },
+    );
+
+    const {file} = ctx.request.files;
+
+    const {Location} = await client
+        .upload({
+            Bucket: IS_OFFLINE ? "local-bucket" : AWS_S3_BUCKET_NAME,
+            Key: `${uuid(
+                AWS_S3_BUCKET_NAME,
+                uuid.URL,
+            )}-${file.name.toLowerCase()}`,
+            Body: createReadStream(file.path),
+            ContentType: getType(file.name),
+            ACL: "public-read",
+        })
+        .promise();
+
+    // eslint-disable-next-line require-atomic-updates
+    ctx.body = {location: Location};
 });
 
 export const handler = sls(app);
